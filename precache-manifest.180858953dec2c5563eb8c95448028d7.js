423c810e26bc26d6d650fce9469487a5f6a4173c
James H. Hehir <jameshhehir@Jamess-MacBook-Pro.local>
2020-10-21 17:54:00 +0100

    Updates

---
+++ precache-manifest.180858953dec2c5563eb8c95448028d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbbb53f81912d589420d49bd1be73e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fb3aa8ea6ba39962d75",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3531d7cffd216ed696c",
+    "url": "/static/js/2.60ef8316.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.60ef8316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb3aa8ea6ba39962d75",
+    "url": "/static/js/main.92b1e9d0.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
