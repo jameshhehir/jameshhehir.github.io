8d8c1e2e521a2fd0bf9586cb642e19aed024b3bd
James H. Hehir <jameshhehir@Jamess-MacBook-Pro.local>
2020-10-21 18:32:08 +0100

    changed mainpage

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -8,16 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Website underconstruction. If you need to contact me use this email <a href="mailto:hello@jimmyhehir.info">hello@jimmyhehir.info</a>
+          Website underconstruction. If you need to contact me use this email <a href="mailto:hello@jimmyhehir.info" style="color: #61dafb;">hello@jimmyhehir.info</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="https://www.linkedin.com/in/jimmy-hehir/"><i class="fab fa-linkedin"></i></a>
+        <a href="https://www.github.com/jameshhehir"><i class="fab fa-github-square"></i></a>
+        <a href="https://www.twitter.com/jimihehir"><i class="fab fa-twitter-square"></i></a>
+        <a href="mailto:jameshhehir@gmail.com"><i class="fas fa-envelope"></i></a>
       </header>
     </div>
   );
