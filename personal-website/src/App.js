4ec314b260a789dced4d5b06abcfac461870254f
James H. Hehir <jameshhehir@Jamess-MacBook-Pro.local>
2020-10-21 18:38:50 +0100

    changed mainpage

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -10,10 +10,6 @@ function App() {
         <p>
           Website underconstruction. If you need to contact me use this email <a href="mailto:hello@jimmyhehir.info" style="color: #61dafb;">hello@jimmyhehir.info</a>
         </p>
-        <a href="https://www.linkedin.com/in/jimmy-hehir/"><i class="fab fa-linkedin"></i></a>
-        <a href="https://www.github.com/jameshhehir"><i class="fab fa-github-square"></i></a>
-        <a href="https://www.twitter.com/jimihehir"><i class="fab fa-twitter-square"></i></a>
-        <a href="mailto:jameshhehir@gmail.com"><i class="fas fa-envelope"></i></a>
       </header>
     </div>
   );
